e39330acf27c19f3f9d2154eb208a475105a2d39
jkaniuka <kan.jan@wp.pl>
2023-05-03 19:00:07 +0200

    deleted collision tags

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lwr_description
+
+```
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/
+git clone https://github.com/jkaniuka/kuka_lwr4plus_urdf.git -b ros2 src/
+source /opt/ros/humble/setup.bash
+colcon build
+. install/setup.bash
+```
--- lwr_description/launch/display.launch.py
+++ lwr_description/launch/display.launch.py
@@ -1,27 +1,26 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
-from launch_ros.actions import Node
 from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
 
 def generate_launch_description():
 
     use_gui_arg = DeclareLaunchArgument(name='use_gui', default_value='true', choices=['true', 'false'],
                                     description='Whether to run joint_state_publisher_gui or not.')
 
-    display_cartesian = Node(
+    display_model = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{
-            'robot_description': ParameterValue(Command(['xacro ', os.path.join(get_package_share_directory('lwr_description'), 'model', 'kuka_lwr2.urdf.xacro') ]), value_type=str),
+            'robot_description': ParameterValue(Command(['xacro ', os.path.join(get_package_share_directory('lwr_description'), 'model', 'kuka_lwr.urdf.xacro') ]), value_type=str),
         }]
         )
 
-    run_rviz_cartesian = Node(
+    run_rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
@@ -51,8 +50,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         use_gui_arg,
-        display_cartesian,
-        run_rviz_cartesian,
+        display_model,
+        run_rviz,
         joint_state_publisher_gui,
         joint_states_cmd
     ])
